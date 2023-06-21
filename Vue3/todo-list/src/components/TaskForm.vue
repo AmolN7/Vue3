e990ff23f7998ea093ebc77d7e6de17adb561ab2
Navale <amol.a.navale@capgemini.com>
2023-06-21 17:35:41 +0530

    updated files

---
+++ Vue3/todo-list/src/components/TaskForm.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="frm">
+      Task Name<span>*</span>
+      <p><input v-model="newTaskName" type="text" placeholder="Task Name" /> </p> 
+      Description<span>*</span>
+      <p><textarea v-model="newTaskDesc" placeholder="Description"></textarea> </p> 
+      Created Date<span>*</span>
+      <p><input :disabled="true"  v-model="newTaskCdate" type="datetime-local" placeholder="Task Name" /> </p> 
+      Due Date<span>*</span>
+      <p><input v-model="newTaskDdate" type="datetime-local" placeholder="Task Name" /> </p> 
+      
+      <div class="btn-holder">
+        <button v-if="!this.$route.params.id" type="button" @click="addTask">Add Task</button>
+        <button v-if="this.$route.params.id" type="button" @click="editTask(this.$route.params.id);">Update Task</button>
+      </div>
+    </div> 
+      
+</template>
+
+<script>
+import { ref } from 'vue';
+import taskData from '../../data/task.json'
+import router from "@/router";
+export default {
+ 
+  setup() {
+    const newTaskName = ref('');    
+    const newTaskDesc = ref('');
+    const newTaskCdate = ref('');
+    const newTaskDdate = ref('');
+    const tasks = ref(taskData);     
+    const tdate = () => { 
+      let d = new Date();      
+      let curr_date = String(d.getDate()).padStart(2, '0');
+      let curr_month = String(d.getMonth()).padStart(2, '0');
+      let curr_year = d.getFullYear();
+      let curr_hours = String(d.getHours()).padStart(2, '0');
+      let curr_min = String(d.getMinutes()).padStart(2, '0');
+      return curr_year+"-"+curr_month+"-"+curr_date+"T"+curr_hours+":"+curr_min;
+    };
+    const showdata = (id=null) => {           
+        newTaskCdate.value = tdate();
+        for (var i = 0; i < tasks.value.length; i++) {
+            if (tasks.value[i].id == id) {            
+                newTaskName.value = tasks.value[i].task_name;                 
+                newTaskDesc.value = tasks.value[i].description;
+                newTaskCdate.value = tasks.value[i].cdate;
+                newTaskDdate.value = tasks.value[i].ddate;                 
+                break;             
+            }  
+        }                
+    };
+    const addTask = () => {          
+       if (newTaskName.value.trim() !== ''           
+           && newTaskDesc.value.trim() !== ''
+           && newTaskCdate.value.trim() !== ''  
+           && newTaskDdate.value.trim() !== ''          
+           ) {
+        const newId = tasks.value.length + 1;
+        tasks.value.push({
+          id: newId,
+          task_name: newTaskName.value,           
+          description: newTaskDesc.value,
+          cdate: newTaskCdate.value,
+          ddate: newTaskDdate.value,
+           
+        });
+        newTaskName.value =  newTaskDesc.value = newTaskCdate.value = newTaskDdate.value ='';
+        
+      }
+      router.push('/task-list');
+    };     
+    const editTask = (id) => { 
+        if (newTaskName.value.trim() !== ''            
+            && newTaskDesc.value.trim() !== ''           
+            && newTaskCdate.value.trim() !== ''           
+            && newTaskDdate.value.trim() !== ''           
+            && id) {
+                for (var i = 0; i < tasks.value.length; i++) {
+                    if (tasks.value[i].id == id) {            
+                        tasks.value[i].task_name= newTaskName.value;                       
+                        tasks.value[i].description= newTaskDesc.value;
+                        tasks.value[i].cdate= newTaskCdate.value;
+                        tasks.value[i].ddate= newTaskDdate.value;                         
+                        break;             
+                    }  
+                }
+                newTaskName.value =  newTaskDesc.value = newTaskCdate.value = newTaskDdate.value ='';
+            }        
+        router.push('/task-list');
+    };
+    
+    
+    return {
+      newTaskName,
+      newTaskCdate,
+      newTaskDesc,
+      newTaskDdate,
+      tasks,
+      addTask,
+      editTask,
+      showdata,
+      tdate,
+    };
+  }, 
+  mounted() {
+    this.showdata(this.$route.params.id);
+  }, 
+    
+};
+</script>
